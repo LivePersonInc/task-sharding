842bd4ebef8de6d977536f3106086da8ad9321e5
eitanya <eitanya@liveperson.com>
2017-04-02 19:08:42 +0300

    support dynamic config in gen-task-shard

--- test/task-shard-test.js
+++ test/task-shard-test.js
@@ -4,35 +4,45 @@ const Events = require('events');
 const GenTaskShard = require('../lib/gen-task-shard');
  
 describe('Task Sharding Tests', () => {
- 
-    it('should fire taskAdded for single task and single node', done => {
-        const clusterMonitoringMock = new Events();
-        const gts = new GenTaskShard({
+    let clusterMonitorMock;
+    let configMonitorMock;
+    let gts;
+
+    beforeEach(()=>{
+        clusterMonitorMock = new Events();
+        configMonitorMock = new Events();
+        gts = new GenTaskShard({
             delay: 0,
-            tasks: [{ id: 1 }],
-            clusterMonitor: clusterMonitoringMock
+            configMonitor: configMonitorMock,
+            clusterMonitor: clusterMonitorMock
         });
-
-        gts.on('taskAdded', (newTaskConf, taskInfoAdder) => {
-            expect(newTaskConf.id).to.equal(1);
-            done();
-        });
-
-        clusterMonitoringMock.emit('clusterChange',"node1",["node1"]);
     });
  
-    it('should fire taskRemoved when a newNode takes the task', done => {
-        const clusterMonitoringMock = new Events();
-        const gts = new GenTaskShard({
-            delay: 0,
-            tasks: [{ id: 1 }],
-            clusterMonitor: clusterMonitoringMock
-        });
-
+    it('should fire taskAdded for single task and single node', done => {
         gts.on('taskAdded', (newTaskConf, taskInfoAdder) => {
-            expect(newTaskConf.id).to.equal(1);
+            expect(newTaskConf.name).to.equal("hello");
+            done();
+        });
+        configMonitorMock.emit('configsAdded',{task1:{ name: "hello" }});
+        clusterMonitorMock.emit('clusterChange',"node1",["node1"]);
+    });
+
+    // depends on current implementation of hashring in which specific key goes to specific node
+    it('should distribute taskAdded between nodes', done => {
+        gts.on('taskAdded', (newTaskConf, taskInfoAdder) => {
+            expect(newTaskConf.name).to.equal("hello");
+            done();
+        });
+        configMonitorMock.emit('configsAdded',{task1:{ name: "hello" }, otherTask:{ name: "goodbye" }});
+        clusterMonitorMock.emit('clusterChange',"node1",["node1","node2","node3"]);
+    });
+ 
+    // depends on current implementation of hashring in which specific key goes to specific node
+    it('should fire taskRemoved when a newNode takes the task', done => {
+        gts.on('taskAdded', (newTaskConf, taskInfoAdder) => {
+            expect(newTaskConf.name).to.equal("hello");
             taskInfoAdder({ myobj: "myobjdata", conf: newTaskConf });
-            clusterMonitoringMock.emit('clusterChange',"node1",["node1","node2","node3"]);
+            clusterMonitorMock.emit('clusterChange',"node1",["node1","node2","node3"]);
         });
 
         gts.on('taskRemoved', (oldTaskInfo) => {
@@ -40,6 +50,32 @@ describe('Task Sharding Tests', () => {
             done();
         });
 
-        clusterMonitoringMock.emit('clusterChange',"node1",["node1"]);
+        configMonitorMock.emit('configsAdded',{otherTask:{ name: "hello" }});
+        clusterMonitorMock.emit('clusterChange',"node1",["node1"]);
+    });
+
+    it('should fire taskAdded after config is added', done => {
+        gts.on('taskAdded', (newTaskConf, taskInfoAdder) => {
+            expect(newTaskConf.name).to.equal("hello");
+            done();
+        });
+        clusterMonitorMock.emit('clusterChange',"node1",["node1"]);
+        configMonitorMock.emit('configsAdded',{task1:{ name: "hello" }});
+    });
+ 
+    it('should fire taskRemoved after config is removed', done => {
+        gts.on('taskAdded', (newTaskConf, taskInfoAdder) => {
+            expect(newTaskConf.name).to.equal("hello");
+            taskInfoAdder({ myobj: "myobjdata", conf: newTaskConf });
+            configMonitorMock.emit('configsRemoved', ["task1"]);
+        });
+
+        gts.on('taskRemoved', (oldTaskInfo) => {
+            expect(oldTaskInfo.myobj).to.equal("myobjdata");
+            done();
+        });
+
+        configMonitorMock.emit('configsAdded',{task1:{ name: "hello" }});
+        clusterMonitorMock.emit('clusterChange',"node1",["node1"]);
     });
 });
